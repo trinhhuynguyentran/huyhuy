48f252c443a0dd2abe0dddfcd9e50101e41f9856
huyntt <ngtthuy@gmail.com>
2023-12-20 14:59:49 +0700

    The first commit

---
+++ BTap/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTap.Models
+{
+    internal class Student
+    {
+    }
+}
